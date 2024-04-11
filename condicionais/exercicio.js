105e44c319eb8ad73d99ee7d3a4510568a7a637d
Narydiniz <senainaryara@gmail.com>
2024-04-11 10:44:41 -0300

    Atualizações js

---
+++ condicionais/exercicio.js
@@ -0,0 +1,4 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+console.log('Diriga sempre usando o cinto de segurança')
+console.log('Você ultrapassou a velocidade permitidada, MULTADO!')
